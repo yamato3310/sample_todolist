1cef43bee2eee254c55a93c0c6c44370b4330242
yamato3310 <yamato3310.tuna@gmail.com>
2020-02-23 16:03:02 +0900

    add jest-puppeteer

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,8 @@
+// See https://github.com/smooth-code/jest-puppeteer for more information about these options
+module.exports = {
+  server: {
+    command: 'npm run serve',
+    port: 8080,
+    launchTimeout: 100000,
+  },
+};
